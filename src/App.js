70a6f2e20942d83f19a26b9c6e4cea0256941d94
KaueFSodate <kauefsodate@gmail.com>
2023-06-12 14:16:18 -0300

    Contador_React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css"
+
+function App() {
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () =>{
+    setContador(contador + 1);
+  }
+
+  const zerar = () =>{
+    setContador(0);
+  }
+  return (
+    <div className="Contador">
+      <h1>Contador</h1>
+      <h2>{contador}</h2>
+      <button onClick={incrementar} className="incrementar">Incrementar</button>
+      <button onClick={zerar} className="zerar">Zerar</button>
+    </div>
+  );
+}
+
+export default App;
